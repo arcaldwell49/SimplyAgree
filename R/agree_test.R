d18772317e7c14f10b479faa4dd88f9fcb42f3e4
Aaron Caldwell <arcaldwell49@gmail.com>
2020-03-06 15:14:44 -0500

    Basic functioning
    agree_test is now bascially working

--- R/agree_test.R
+++ R/agree_test.R
@@ -1,53 +1,58 @@
-#' Design function used to specify the parameters to be used in simulations
-#' @param design String specifying the ANOVA design.
-#' @param n Sample size in each condition
-#' @param mu Vector specifying mean for each condition
-#' @param sd standard deviation for all conditions (or a vector specifying the sd for each condition)
-#' @param r Correlation between dependent variables (single value or matrix)
-#' @param labelnames Optional vector to specifying factor and condition names (recommended, if not used factors and levels are indicated by letters and numbers)
-#' @param plot Should means plot be printed (defaults to TRUE)
-#' @return Returns single list with simulated data, design, design list, factor names, formulas for ANOVA, means, sd, correlation, sample size per condition, correlation matrix, covariance matrix, design string, labelnames, labelnameslist, factor names, meansplot
+#' Tests for Absolute Agreement
+#' @param x Criterion measurement, or first measurement if repeated measures
+#' @param y Other measurement, or second measurement if repeated measures
+#' @param LoA Limit of Agreement between 0 and 1; .95 = 95\% limit of agreement.
+#' @param delta Equivalence Bound for Agreement.
+#' @param rep.measure logical value; if TRUE then x & y are repeated measures across subjects
+#' @param alpha Optional vector to specifying factor and condition names (recommended, if not used factors and levels are indicated by letters and numbers
+#' @return Returns single list with the results of the agreement analysis.
 #'
 #' \describe{
-#'   \item{\code{"dataframe"}}{A sample dataframe of what data could look like given the proposed parameters.}
-#'   \item{\code{"design"}}{\code{aov} The design string, e.g. "2b*2w".}
-#'   \item{\code{"design_list"}}{The list of variables in the design.}
-#'   \item{\code{"frml1"}}{The first formula created for this design.}
-#'   \item{\code{"frml2"}}{The second formula created for this design.}
-#'   \item{\code{"mu"}}{Vector of means.}
-#'   \item{\code{"sd"}}{Vector of standard deviations.}
-#'   \item{\code{"r"}}{Common correlation coefficient.}
-#'   \item{\code{"n"}}{Sample size per cell. Can be entered as a single value or list of sample sizes for each condition. If unequal n is entered then the design can only be passed onto ANOVA_power.}
-#'   \item{\code{"cor_mat"}}{The correlation matrix.}
-#'   \item{\code{"sigmatrix"}}{The variance-covariance matrix.}
-#'   \item{\code{"design_factors"}}{Total number of within-subjects factors.}
-#'   \item{\code{"labelnames"}}{List of the label names.}
-#'   \item{\code{"labelnameslist"}}{Secondary list of labelnames}
-#'   \item{\code{"factornames"}}{List of the factor titles.}
-#'   \item{\code{"meansplot"}}{Plot of the experimental design.}
+#'   \item{\code{"shieh_test"}}{The TOST hypothesis test as described by Shieh.}
+#'   \item{\code{"ccc.xy"}}{Lin's concordance correlation coefficient and confidence intervals.}
+#'   \item{\code{"s.shift"}}{Scale shift from x to y.}
+#'   \item{\code{"l.shift"}}{Location shift from x to y.}
+#'   \item{\code{"bias"}}{a bias correction factor that measures how far the best-fit line deviates from a line at 45 degrees. No deviation from the 45 degree line occurs when bias = 1. See Lin 1989, page 258.}
+#'   \item{\code{"df_diff"}}{a data frame with 4 columns: x = data column x from arguments, y = data column y from agruments, mean = the mean of each pair of measurements, delta = vector y minus vector x.}
+#'   \item{\code{"delta"}}{a data frame listing the average difference between the two sets of measurements, the standard deviation of the difference between the two sets of measurements and the lower and upper confidence limits of the difference between the two sets of measurements. If rep.measure == TRUE the confidence interval of the difference is adjusted to account for repeated observations across individual subjects.}
+#'   \item{\code{"identity.plot"}}{Plot of x and y with a line of identity with a linear regression line}
+#'   \item{\code{"bland_alt.plot"}}{Simple Bland-Altman plot. Red line are the upper and lower bounds for shieh test; grey box is the acceptable limits (delta). If the red lines are within the grey box then the shieh test should indicate 'reject h0', or to reject the null hypothesis that this not acceptable agreement between x & y.}
 #'
 #' }
 
-#' @examples
+#' @examples to be added
 #'
-#' @section Warnings:
-#' Varying the sd or r (e.g., entering multiple values) violates assumptions of homoscedascity and sphericity respectively
-#' @importFrom stats pnorm pt qnorm qt as.formula median
+#' @section References:
+#' Gwowen Shieh (2019): Assessing Agreement Between Two Methods of Quantitative Measurements: Exact Test Procedure and Sample Size Calculation, Statistics in Biopharmaceutical Research, <https://doi.org/10.1080/19466315.2019.1677495>
+#' @importFrom stats pnorm pt qnorm qt lm
 #' @import ggplot2
 #' @export
 #'
 
 
-agree_test <- function (data, x,y,
-  alpha=0.05,prop0=0.8,delta=.1,n=15,xbar=0.011,s=0.044) {
+agree_test <- function (x,y,LoA=0.8,delta=.1,
+                        rep.measure=FALSE,
+                        alpha=0.05,
+                        verbose = FALSE) {
+  if(LoA >=1 || LoA <=0){
+    stop("Limit of Agreement (LoA) must be a value between 0 and 1")
+  }
   #USER SPECIFICATIONS PORTION
   #alpha<-0.05 #DESIGNATED ALPHA
-  #prop0<-0.8 #NULL CENTRAL PROPORTION
+  #prop0<-0.8 #NULL CENTRAL PROPORTION or Limit of Agreement
   #delta<-0.1 #THRESHOLD
   #n<-15 #SAMPLE SIZE
   #xbar<-0.011 #SAMPLE MEAN
   #s<-0.044 #SAMPLE STANDARD DEVIATION
   #END OF SPECIFICATION
+  prop0 = LoA
+  conf.level=(1-alpha)
+  ccc_res = ccc.xy(x,y,rep.measure=rep.measure, conf.level=(1-alpha))
+  #pull values from ccc function output
+  xbar = ccc_res$delta$est #mean delta
+  s = ccc_res$delta$delta.sd #sd of delta
+  n = nrow(ccc_res$df_diff)
+
   pct<-1-(1-prop0)/2
   zp<-qnorm(pct)
   df<-n-1
@@ -60,29 +65,107 @@ agree_test <- function (data, x,y,
   intl<-int/numint
   cvec<-cl+intl*(0:numint)
   wcpdf<-(intl/3)*coevec*dchisq(cvec,df)
-  fungam<-function () {
-    gaml<-0
-    gamu<-100
-    loop<-0
-    dalpha<-1
-    while(abs(dalpha)>1e-8 | dalpha<0){
-      gam<-(gaml+gamu)/2
-      h<-zp*sqrt(n)-gam*sqrt(cvec/df)
-      ht<-h*(cvec<n*df*(zp/gam)^2)
-      alphat<-sum(wcpdf*(2*pnorm(ht)-1))
-      if (alphat>alpha) gaml<-gam else gamu<-gam
-      loop<-loop+1
-      dalpha<-alphat-alpha
-    }
-
-    return(gam)
+  gaml<-0
+  gamu<-100
+  loop<-0
+  dalpha<-1
+  while(abs(dalpha)>1e-8 | dalpha<0){
+    gam<-(gaml+gamu)/2
+    h<-zp*sqrt(n)-gam*sqrt(cvec/df)
+    ht<-h*(cvec<n*df*(zp/gam)^2)
+    alphat<-sum(wcpdf*(2*pnorm(ht)-1))
+    if (alphat>alpha) gaml<-gam else gamu<-gam
+    loop<-loop+1
+    dalpha<-alphat-alpha
   }
-  gam<-fungam()
   el<-xbar-gam*stdh
   eu<-xbar+gam*stdh
   rej<-(-delta<el)*(eu<delta)
-  if (rej==1) test=c('reject ho') else test=("don't reject h0")
-  print(c('gam, el, eu'))
-  print(c(gam,el,eu),digits=3)
-  print(c('test:',test))
+  rej_text = "don't reject h0"
+  if(rej==1){
+    rej_text = "reject h0"
+  }
+
+  shieh_test = data.frame(prop0,el,eu,rej_text,gam)
+  names(shieh_test) = c("LoA","lower.ci","upper.ci", "h0_test","test_statistic")
+
+  #######################
+  # Plot Results ----
+  #######################
+
+  z <- lm(y ~ x)
+  the_int <- summary(z)$coefficients[1,1]
+  the_slope <-  summary(z)$coefficients[2,1]
+  tmp.lm <- data.frame(alpha, beta)
+
+  identity.plot = ggplot(ccc_res$df_diff, aes(x = x, y = y)) +
+    geom_point(na.rm = TRUE) +
+    geom_abline(intercept = 0, slope = 1) +
+    geom_abline(
+      data = tmp.lm,
+      aes(intercept = the_int, slope = the_slope),
+      linetype = "dashed",
+      color = "red"
+    ) +
+    xlab("Method: x") +
+    ylab("Method: y") +
+    coord_fixed(ratio = 1 / 1) +
+    theme_bw()
+
+  bor = delta
+  neg.bor = -1*bor
+  bland_alt.plot =  ggplot(ccc_res$df_diff, aes(x = mean, y = delta)) +
+    geom_point(na.rm = TRUE) +
+    geom_ribbon(aes(ymin = neg.bor, ymax = bor),
+                alpha = .1) +
+    geom_hline(data = shieh_test,
+               aes(yintercept = lower.ci, color = "red"),
+               linetype = 2) +
+    geom_hline(data = shieh_test,
+               aes(yintercept = upper.ci, color = "red"),
+               linetype = 2) +
+    geom_hline(data = ccc_res$delta,
+               aes(yintercept = est),
+               linetype = 1) +
+    xlab("Average of Method x and Method y") +
+    ylab("Difference between Methods x & y") +
+    theme_bw() +
+    theme(legend.position = "none")
+
+
+  #######################
+  # Return Results ----
+  #######################
+
+  if (verbose == TRUE) {
+    # The section below should be blocked out when in Shiny
+
+    cat(100*LoA,"% Limits of Agreement |"," Null Central Proportion = ", LoA,  sep = "")
+    cat("\n")
+    cat("alpha =", alpha, "|", (1-alpha)*100,"% Confidence Interval")
+    cat("\n")
+    cat("### TOST Results ###")
+    cat("\n")
+    cat("Exact C.I.:"," [",round(el,4),", ",round(eu,4),"]",sep="")
+    cat("\n")
+    cat("test: ",rej_text,sep="")
+    cat("\n")
+    cat("### Concordance Correlation Coefficient (CCC) ###")
+    cat("\n")
+    cat("CCC: ",round(ccc_res$rho.c$est.ccc,4),", ",100*conf.level,"% C.I. ","[",round(ccc_res$rho.c$lower.ci,4),", ",round(ccc_res$rho.c$upper.ci,4),"]",sep="")
+
+  }
+
+  res_list <- list(shieh_test = shieh_test,
+                   ccc.xy = ccc_res$rho.c,
+                   s.shift = ccc_res$s.shift,
+                   l.shift = ccc_res$l.shift,
+                   bias = ccc_res$bias,
+                   df_diff = ccc_res$df_diff,
+                   delta = ccc_res$delta,
+                   bland_alt.plot = bland_alt.plot,
+                   identity.plot = identity.plot)
+
+
+
 }
--- R/other functions.R
+++ R/other functions.R
@@ -1,7 +1,7 @@
-ccc.xy <- function(x,y) {
-  
-  
-  
+ccc.xy <- function(x,y, rep.measure, conf.level) {
+
+  N. <- 1 - ((1 - conf.level)/2)
+  zv <- qnorm(N., mean = 0, sd = 1)
   dat <- data.frame(x, y)
   id <- complete.cases(dat)
   nmissing <- sum(!complete.cases(dat))
@@ -17,8 +17,59 @@ ccc.xy <- function(x,y) {
   sl <- r * sd1/sd2
   sxy <- r * sqrt(sx2 * sy2)
   p <- 2 * sxy/(sx2 + sy2 + (yb - xb)^2)
-  
-  return(p)
-  
+
+  delta <- (dat$x - dat$y)
+  rmean <- apply(dat, MARGIN = 1, FUN = mean)
+  blalt <- data.frame(x=dat$x,y=dat$y,mean = rmean, delta)
+  v <- sd1/sd2
+  u <- (yb - xb)/((sx2 * sy2)^0.25)
+  C.b <- p/r
+  sep <- sqrt(((1 - ((r)^2)) * (p)^2 * (1 - ((p)^2))/(r)^2 +
+                 (2 * (p)^3 * (1 - p) * (u)^2/r) - 0.5 * (p)^4 * (u)^4/(r)^2)/(k -
+                                                                                 2))
+  ll <- p - (zv * sep)
+  ul <- p + (zv * sep)
+  t <- log((1 + p)/(1 - p))/2
+  set = sep/(1 - ((p)^2))
+  llt = t - (zv * set)
+  ult = t + (zv * set)
+  llt = (exp(2 * llt) - 1)/(exp(2 * llt) + 1)
+  ult = (exp(2 * ult) - 1)/(exp(2 * ult) + 1)
+  if (rep.measure == TRUE) {
+    dat$sub <- subjectid
+    if (!is.factor(dat$sub))
+      dat$sub <- as.factor(dat$sub)
+    nsub <- length(levels(dat$sub))
+    model <- aov(delta ~ dat$sub)
+    MSB <- anova(model)[[3]][1]
+    MSW <- anova(model)[[3]][2]
+    pairs <- NULL
+    for (i in 1:nsub) {
+      pairs[i] <- sum(is.na(delta[dat$sub == levels(dat$sub)[i]]) ==
+                        FALSE)
+    }
+    sig.dl <- (MSB - MSW)/((sum(pairs)^2 - sum(pairs^2))/((nsub -
+                                                             1) * sum(pairs)))
+    delta.sd <- sqrt(sig.dl + MSW)
+  }
+  if (rep.measure == FALSE) {
+    delta.sd <- sqrt(var(delta, na.rm = TRUE))
+  }
+  ba.p <- mean(delta)
+
+  sblalt <- data.frame(est = ba.p, delta.sd = delta.sd)
+
+  rho.c <- data.frame(p, llt, ult)
+  names(rho.c) <- c("est.ccc", "lower.ci", "upper.ci")
+
+  res_list <- list(rho.c = rho.c, s.shift = v, l.shift = u,
+                   bias = C.b, df_diff = blalt, delta = sblalt,
+                   nmissing = nmissing)
+  #s.shift = scale (variance) shift
+  #l.shift = change in mean
+  #bias = bias correction (1 = perfect)
+  #mean.dlt = mean and delta pairs data frame
+  #sblalt =
+
 }
 
