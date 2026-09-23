c9ce9e724f694b2d61a2a83311ffa9f73d2084e7
Aaron Caldwell <arcaldwell49@gmail.com>
2026-09-23 11:22:24 -0500

    tested in jamovi

--- R/jmvreli.h.R
+++ R/jmvreli.h.R
@@ -150,7 +150,8 @@ jmvreliBase <- if (requireNamespace("jmvcore", quietly=TRUE)) R6::R6Class(
                 revision = revision,
                 pause = NULL,
                 completeWhenFilled = FALSE,
-                requiresMissings = FALSE)
+                requiresMissings = FALSE,
+                weightsSupport = 'auto')
         }))
 
 #' Reliability Analysis
